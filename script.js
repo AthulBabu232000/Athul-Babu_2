2ab2f3e50832e65acbf244eb4c5155219170115e
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-05-13 22:42:36 +0530

    added cards

---
+++ script.js
@@ -0,0 +1,13 @@
+// const switcher = (el) => {
+
+//     var nav = document.querySelectorAll('.nav-item')
+//     nav.forEach((ch) =>{
+//         var name = ch.classList
+
+// if(name .includes("active")){
+
+//   alert('nyc')
+// }
+//     })
+// }
+
