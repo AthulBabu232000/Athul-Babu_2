3bbb65c6ccdebcf038446334967b49c8e9595a05
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-05-14 15:12:33 +0530

    added msg boxes

--- script.js
+++ script.js
@@ -11,3 +11,18 @@
 //     })
 // }
 
+const modalzoom = () =>{
+    var modal = document.getElementById('modelId')
+    modal.style.animation = 'animateModal 2s forwards'
+}
+
+const modalout = () =>{
+    var modal = document.getElementById('modelId');
+    modal.style.animation = 'animateModout 2s forwards'
+    setTimeout(() => {
+
+
+        window.location.href = 'index.html';
+    }, 2000);
+
+}
